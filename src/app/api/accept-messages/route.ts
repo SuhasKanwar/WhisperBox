f49355a3e72c0661d7172d8adb971fa3377de99a
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-08 23:08:35 +0530

    Added the POST route for the accept-messages api'

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -1,1 +1,63 @@
 import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/options';
+import dbConnect from '@/src/lib/dbConnect';
+import UserModel from '@/src/models/User';
+import { User } from 'next-auth';
+
+
+export async function POST(request: Request){
+    await dbConnect();
+
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+    if(!session || !session.user){
+        return Response.json(
+            {
+                success: false,
+                message: "User not authenticated"
+            },
+            {
+                status: 401
+            }
+        );
+    }
+
+    const userID = user._id;
+    const { acceptMessages } = await request.json();
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(userID, { isAcceptingMessages: acceptMessages }, { new: true });
+        if(!updatedUser){
+            return Response.json(
+                {
+                    success: false,
+                    message: "Failed to update the status code to accept messages"
+                },
+                {
+                    status: 401
+                }
+            );
+        }
+        return Response.json(
+            {
+                success: true,
+                message: "Message acceptance status updated successfully"
+            },
+            {
+                status: 200
+            }
+        );
+    }
+    catch(err){
+        console.error("Failed to update the status code to accept messages", err);
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to update the status code to accept messages"
+            },
+            {
+                status: 500
+            }
+        );
+    }
+}

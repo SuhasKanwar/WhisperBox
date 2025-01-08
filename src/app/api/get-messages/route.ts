09358b637a20e301bc00861093ab02707b1db18f
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-08 23:31:20 +0530

    Message retreivel API completed

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -31,7 +31,37 @@ export async function GET(request: Request){
             { $sort: { 'messages.createdAt': -1 } },
             { $group: { _id: '$_id', messages: { $push: '$messages' } } }
         ]);
+        if(!user || user.length === 0){
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                {
+                    status: 404
+                }
+            );
+        }
+        return Response.json(
+            {
+                success: true,
+                message: "Messages retrieved",
+                messages: user[0].messages
+            },
+            {
+                status: 200
+            }
+        );
     }
     catch(err){
+        return Response.json(
+            {
+                success: false,
+                message: "Error retrieving messages"
+            },
+            {
+                status: 500
+            }
+        );
     }
 }

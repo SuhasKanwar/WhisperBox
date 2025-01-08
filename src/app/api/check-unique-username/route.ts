879342913acadbe4d3a6d6559e3cd15d2aa99c59
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-08 21:08:39 +0530

    Setting for up unique username

---
+++ src/app/api/check-unique-username/route.ts
@@ -0,0 +1,5 @@
+import dbConnect from "@/src/lib/dbConnect";
+import UserModel from "@/src/models/User";
+import { z } from "zod";
+import { usernameValidation } from "@/src/schemas/signUpSchema";
+

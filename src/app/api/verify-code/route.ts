d406ba2d46e821f05b4b2ed7cc3786add0b4d6d1
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-08 21:50:22 +0530

    OTP verification setup complete

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -2,3 +2,73 @@ import dbConnect from "@/src/lib/dbConnect";
 import UserModel from "@/src/models/User";
 import { verifySchema } from "@/src/schemas/verifySchema";
 
+export async function POST(request: Request){
+    await dbConnect();
+    try{
+        const { username, otp } = await request.json();
+        const decodedUsername = decodeURIComponent(username);
+        const user = await UserModel.findOne({ username: decodedUsername });
+
+        if(!user){
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                {
+                    status: 404
+                }
+            );
+        }
+
+        const isOtpValid = user.otp === otp;
+        const isOtpNotExpired = new Date() < new Date(user.otpExpiry);
+        if(isOtpValid && isOtpNotExpired){
+            user.isVerified = true;
+            await user.save();
+            return Response.json(
+                {
+                    success: true,
+                    message: "Account verified successfully"
+                },
+                {
+                    status: 200
+                }
+            );
+        }
+        else if(!isOtpNotExpired){
+            return Response.json(
+                {
+                    success: false,
+                    message: "OTP has expired"
+                },
+                {
+                    status: 400
+                }
+            );
+        }
+        else{
+            return Response.json(
+                {
+                    success: false,
+                    message: "Invalid OTP"
+                },
+                {
+                    status: 400
+                }
+            );
+        }
+    }
+    catch(err){
+        console.error("Error verifying user: ", err);
+        return Response.json(
+            {
+                success: false,
+                message: "Error verifying user"
+            },
+            {
+                status: 500
+            }
+        );
+    }
+}

62d2986b8d84dc52d21c7aeee2d863c6b73f8882
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-09 17:10:22 +0530

    Setting up for messages page of the user

---
+++ src/app/u/[username]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserMessagesPage = () => {
+    return (
+        <div>
+            <h1>User Messages Page</h1>
+        </div>
+    );
+}
+
+export default UserMessagesPage;

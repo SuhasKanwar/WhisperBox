b2dc524136da453ecef76d1dfa4b839d57581597
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-09 15:23:40 +0530

    Added the logical part of the otp

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,10 +1,53 @@
+import { useToast } from '@/hooks/use-toast';
+import { verifySchema } from '@/src/schemas/verifySchema';
+import { ApiResponse } from '@/src/types/ApiResponse';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { useParams } from 'next/navigation';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
 const VerifyAccountPage = () => {
-  return (
-    <div>
-    </div>
-  )
+    const router = useRouter();
+    const params = useParams<{username: string}>();
+    const { toast } = useToast();
+
+    const form = useForm<z.infer<typeof verifySchema>>({
+        resolver: zodResolver(verifySchema)
+    });
+
+    const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+        try{
+            const response = await axios.post(`/api/verify-code`, {
+                username: params.username,
+                code: data.code
+            });
+
+            toast({
+                title: "Success",
+                description: response.data.message,
+            });
+
+            router.replace('/sign-in');
+        }
+        catch(err){
+            console.error("Error verifying account", err);
+            const axiosError = err as AxiosError<ApiResponse>;
+            let errorMessage = axiosError.response?.data.message || "An error occurred verifying account";
+            toast({
+                title: "Verification Failed",
+                description: errorMessage,
+                variant: "destructive"
+            });
+        }
+    }
+
+    return (
+        <div>
+        </div>
+    );
 }
 
 export default VerifyAccountPage;

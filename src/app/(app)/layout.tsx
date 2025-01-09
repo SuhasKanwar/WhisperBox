193447d5cc7c40b24be05848c209cc35a34a7505
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-09 17:04:21 +0530

    Refactoring folder structure

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import AuthProvider from "../context/AuthProvider";
+import "../globals.css";
+import AuthProvider from "../../context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {

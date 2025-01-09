26896e3abb53223abc84371086956acfb08ecd66
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-09 14:03:01 +0530

    Done with the logical part of the sign-in page

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -5,7 +5,7 @@ export const usernameValidation = z
     .min(2, "Username must be atleast 2 characters")
     .max(20, "Username must not be more than 20 characters")
 
-export const signUpScheam = z.object({
+export const signUpSchema = z.object({
     username: usernameValidation,
     email: z.string().email({ message: "Invalid email address" }),
     password: z.string().min(6, { message: "Message must be at least 6 characters" })

f85547bc153ae8d181cb2a39a4fd02c62e0f2265
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-09 20:35:02 +0530

    Setting up the index page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,18 @@
-export default function IndexPage() {
+import React from "react";
+
+const IndexPage = () => {
   return (
-    <div>
-      <h1>WhisperBox</h1>
-    </div>
+    <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+      <section className="text-center mb-8 md:mb-12">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Dive into the World of Anonymous Conversations
+        </h1>
+        <p className="mt-3 md:mt-4 text-base md:text-lg">
+          Explore Mystery Message - Where your identity remains a secret.
+        </p>
+      </section>
+    </main>
   );
 }
+
+export default IndexPage;

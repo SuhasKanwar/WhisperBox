316e3bd9a8774ce82cba29030b1eed49c513e4d0
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-09 18:36:29 +0530

    Completed Message Card UI

--- components/MessageCard.tsx
+++ components/MessageCard.tsx
@@ -3,35 +3,59 @@
 import React from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogDescription, AlertDialogFooter, AlertDialogCancel, AlertDialogAction } from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+import { Message } from "@/src/models/Messages";
+import { useToast } from "@/hooks/use-toast";
+import axios from "axios";
+import { ApiResponse } from "@/src/types/ApiResponse";
 
-const MessageCard = () => {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle></CardTitle>
+type MessageCardProps = {
+    message: Message;
+    onMessageDelete: (messageId: string) => void;
+}
 
-        <AlertDialog>
-          <AlertDialogTrigger>Open</AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
+    const { toast } = useToast();
 
-        <CardDescription></CardDescription>
-      </CardHeader>
-      <CardContent></CardContent>
-    </Card>
-  );
+    const handleDelete = async () => {
+        const response = await axios.delete<ApiResponse>(`/api/messages/${message._id}`);
+        toast({
+            title: response.data.message
+        })
+        let mssgID = message._id as string;
+        onMessageDelete(mssgID);
+    }
+
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle></CardTitle>
+    
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant='destructive'><X className="w-5 h-5"/></Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete your
+                  message and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+    
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    );
 };
 
 export default MessageCard;

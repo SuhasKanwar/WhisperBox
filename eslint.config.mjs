9a54ab4fabbc1b0c8581d98c27025c2cb138dce2
SuhasKanwar <suhas.kanwar@gmail.com>
2025-01-15 17:36:17 +0530

    Updated esling config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: import.meta.dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: ["**/*.ts", "**/*.tsx"],
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-      "@typescript-eslint/no-explicit-any": "off",
-      "react/no-unescaped-entities": "off",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
-  },
-];
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
